c1b3efcc9001124f2bcf8561b0a1d2230eae193e
Grzegorz Wojciechowski <grliszas14@gmail.com>
2019-03-26 20:54:52 +0100

    Setting up Firebase
    * Added to Gradle
    * Made empty activity
    * Wrote some commented example code

---
+++ app/src/main/java/com/example/fairride/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fairride
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.client.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /* Setup Firebase */
+        //Firebase.setAndroidContext(this);
+
+        /* Read & write to Firebase Database */
+        // Firebase myFirebaseRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
+
+        // write
+        //myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        // read
+        // myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+        //  @Override
+        //  public void onDataChange(DataSnapshot snapshot) {
+        //    System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+        //  }
+        //  @Override public void onCancelled(FirebaseError error) { }
+        //});
+
+    }
+}
