a47634e360180745dac3c69f970dc51c6ef72450
Grzegorz Wojciechowski <grliszas14@gmail.com>
2019-05-01 12:40:17 +0200

    Activities:
    * Zmienione główne activity
    * Dodane activity dla kierowcy i pasażera
    * Google maps do poprawki, dodany klucz, ale mapka się jeszcze nie
    wyświetla

--- app/src/main/java/com/example/fairride/MainActivity.kt
+++ app/src/main/java/com/example/fairride/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fairride
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.client.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /* Setup Firebase */
-        Firebase.setAndroidContext(this);
-        println("dupa");
+        Firebase.setAndroidContext(this)
+        println("dupa")
+
+        driverButton.setOnClickListener{
+            val intent = Intent(this, DriverSettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        passengerButton.setOnClickListener {
+            val intent = Intent(this, PassengerActivity::class.java)
+            startActivity(intent)
+        }
 
         /* Read & write to Firebase Database */
         // Firebase myFirebaseRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
