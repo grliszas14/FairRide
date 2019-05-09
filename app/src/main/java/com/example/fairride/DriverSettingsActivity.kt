35877caad9fcaf66d841b22d127f03aa5f80fb34
Grzegorz Wojciechowski <grliszas14@gmail.com>
2019-05-09 23:19:06 +0200

    Dodany listView z autami (na razie hardcoded)
    * Do tego wrzucilem randomowy JSON do firebase database

--- app/src/main/java/com/example/fairride/DriverSettingsActivity.kt
+++ app/src/main/java/com/example/fairride/DriverSettingsActivity.kt
@@ -2,11 +2,26 @@ package com.example.fairride
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class DriverSettingsActivity : AppCompatActivity() {
+
+    lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_settings)
+
+        listView = findViewById(R.id.listView)
+
+        val cars = arrayOf("Audi", "BMW", "Opel", "Seat")
+
+        listView.adapter =  ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cars)
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(applicationContext, "You chose: " + cars[i], Toast.LENGTH_SHORT).show()
+        }
     }
 }
