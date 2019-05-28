f166625246166510ecf0f63af7fde5c7ad089d0f
Grzegorz Wojciechowski <grliszas14@gmail.com>
2019-05-28 18:38:08 +0200

    Okienko podsumowania dla kierowcy

---
+++ app/src/main/java/com/example/fairride/DriverSummaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fairride
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DriverSummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver_summary)
+    }
+}
